5c1e35811bc7fcc9b8473ae23443a7d7ea590477
finite-automaton <71351488+finite-automaton@users.noreply.github.com>
2022-10-14 21:49:32 +0100

    configure webpack

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ var moment = require("moment");
 console.log("Hello from javascript!");
 console.log(moment().startOf('day').fromNow());
 console.log(moment().endOf('day').fromNow());
+console.log(x);
---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mode: 'development',
+  entry: './index.js',
+  output: {
+    filename: 'mains.js'
+  },
+  devtool: "source-map"
+};
